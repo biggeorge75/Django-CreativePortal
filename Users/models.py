943d93cd27aeccb7665e517a7a435f4cf896ab6a
ted <apple.tibor@gmail.com>
2019-12-01 18:53:37 +0100

    1-2 finish

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200, blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pictures", blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=200, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    facebook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        if self.profile_pic:
+            image_path = self.profile_pic.path
+            img = Image.open(image_path)
+
+            if img.size[0] > 300 or img.size[1] > 300:
+                img.thumbnail((300, 300))
+                img.save(image_path)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+    instance.profile.save()
--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,21 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from .forms import ProfileForm
+from .models import Profile
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/registration.html"
-    success_url = "/accounts/login/"
+    success_url = reverse_lazy("login")
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = "users/profile.html"
+    success_url = reverse_lazy("home")
+    login_url = reverse_lazy("login")
